059ca9b2e30b875ce5401bb7902f98c229c9f43a
Wilson Junior <wilson.assis.junior@gmail.com>
2024-06-18 21:12:44 -0300

    Criando a entidade customer

---
+++ Aluno/src/entity/customer.ts
@@ -0,0 +1,11 @@
+class Customer {
+    _id: string;
+    _name: string;
+    _address: string;
+
+    constructor(id: string, name: string, address:string) {
+        this._id = id;
+        this._name = name;
+        this._address = address;
+    }
+}
