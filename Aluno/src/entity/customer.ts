0d4fc814b63b7838e53b69006bad3b4d9a38ee72
Wilson Junior <wilson.assis.junior@gmail.com>
2024-06-25 21:08:31 -0300

    Value objects na prática

--- Aluno/src/entity/customer.ts
+++ Aluno/src/entity/customer.ts
@@ -1,11 +1,26 @@
+import Address from "./address";
+
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
+    _address!: Address;
 
-    constructor(id: string, name: string, address:string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
+        this.validate();
+    }
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+    }
+
+    set Address(address: Address) {
         this._address = address;
     }
 }
