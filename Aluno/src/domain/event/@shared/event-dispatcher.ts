81ba07d7433d39e9fe9424cbc556a9a4974b5b61
Wilson Junior <wilson.assis.junior@gmail.com>
2024-09-11 21:03:16 -0300

    Registrando primeiro evento

--- Aluno/src/domain/event/@shared/event-dispatcher.ts
+++ Aluno/src/domain/event/@shared/event-dispatcher.ts
@@ -2,10 +2,19 @@ import EventDispatcherInterface from "./event-dispatcher.interface";
 import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
-export default class EventDispatcher implements EventDispatcherInterface {
-    
+export default class EventDispatcher implements EventDispatcherInterface {    
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] }{
+        return this.eventHandlers;
+    }
+
     register(eventName: string, eventHandler: EventHandlerInterface): void {
-        throw new Error("Method not implemented.");
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+
+        this.eventHandlers[eventName].push(eventHandler);
     }
 
     unregister(eventName: string, eventHandler: EventHandlerInterface): void {
