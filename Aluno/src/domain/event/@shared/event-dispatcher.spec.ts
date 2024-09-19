704b43e3b1d129a95166156714b6100885bdc37e
Wilson Junior <wilson.assis.junior@gmail.com>
2024-09-19 20:32:52 -0300

    Correção ProductCreatedEvent

--- Aluno/src/domain/event/@shared/event-dispatcher.spec.ts
+++ Aluno/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -4,12 +4,12 @@ import EventDispatcher from "./event-dispatcher";
 
 describe("Domain events tests", () => {
     
-    it("Should register event handler", () => {
+    it("Should register an event handler", () => {
 
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
 
-        eventDispatcher.register("ProductCreateEvent", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
@@ -21,13 +21,13 @@ describe("Domain events tests", () => {
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
         
-        eventDispatcher.register("ProductCreateEvent", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
 
-        eventDispatcher.unregister("ProductCreateEvent", eventHandler);
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
 
-        expect(eventDispatcher.getEventHandlers["ProductCreateEvent"]).toBeDefined();
-        expect(eventDispatcher.getEventHandlers["ProductCreateEvent"].length).toBe(0);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
     });
 
     it("Should unregister all event handlers", () => {
@@ -35,7 +35,7 @@ describe("Domain events tests", () => {
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
         
-        eventDispatcher.register("ProductCreateEvent", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
 
         eventDispatcher.unregisterAll();
@@ -49,7 +49,7 @@ describe("Domain events tests", () => {
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
         const spyEventHandler = jest.spyOn(eventHandler, "handle");
 
-        eventDispatcher.register("ProductCreateEvent", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
 
         const productCreatedEvent = new ProductCreatedEvent({
