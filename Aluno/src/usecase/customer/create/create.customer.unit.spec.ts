a42fe0fa9ec13113128b6f9b1205623f2ec355e4
Wilson Junior <wilson.assis.junior@gmail.com>
2025-03-31 22:09:59 -0300

    Preparando teste de criação de Customer

---
+++ Aluno/src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,38 @@
+const input = {
+    name: "John",
+    address: {
+        street: "Street",
+        number: 123,
+        zip: "Zip",
+        city: "City",
+    },
+};
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+};
+
+describe("Unit test create customer use case", () => {
+    it("should create a customer", async () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CustomerCreateUseCase(customerRepository);
+
+        const output = await customerCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            address: {
+                street: input.address.street,
+                number: input.address.number,
+                zip: input.address.zip,
+                city: input.address.city,
+            },
+        });
+    });
+});
