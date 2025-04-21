05ccc00664a0c7a4619f6cf0d213c5315df71a3b
Wilson Junior <wilson.assis.junior@gmail.com>
2025-04-21 16:46:05 -0300

    Configurando o presenter para retornar também no formato xml

--- Aluno/src/infrastructure/api/presenters/customer.presenter.ts
+++ Aluno/src/infrastructure/api/presenters/customer.presenter.ts
@@ -2,34 +2,30 @@ import { toXML } from "jstoxml";
 import { OutputListCustomerDto } from "../../../usecase/customer/list/list.customer.dto";
 
 export default class CustomerPresenter {
+  static listXML(data: OutputListCustomerDto): string {
+    const xmlOption = {
+      header: true,
+      indent: "  ",
+      newline: "\n",
+      allowEmpty: true,
+    };
 
-    static toXML(customer: OutputListCustomerDto): string {
-
-        const xmlOption = {
-            header: true,
-            indent: "  ",
-            newLine: "\n",
-            allowEmpty: true,
-        };
-
-        const xml = toXML(
-            {
-                customers: {
-                    customer: customer.customers.map((customer) => ({
-                        id: customer.id,
-                        name: customer.name,
-                        address: {
-                            street: customer.address.street,
-                            number: customer.address.number,
-                            zipCode: customer.address.zip,
-                            city: customer.address.city,
-                        },
-                    })),
-                },
+    return toXML(
+      {
+        customers: {
+          customer: data.customers.map((customer) => ({
+            id: customer.id,
+            name: customer.name,
+            address: {
+              street: customer.address.street,
+              number: customer.address.number,
+              zip: customer.address.zip,
+              city: customer.address.city,
             },
-            xmlOption
-        );
-
-        return xml;
-    }
+          })),
+        },
+      },
+      xmlOption
+    );
+  }
 }
--- Aluno/src/infrastructure/api/routes/customer.route.ts
+++ Aluno/src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import CreateCustomerUseCase from '../../../usecase/customer/create/create.customer.usecase';
 import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
 import ListCustomerUseCase from '../../../usecase/customer/list/list.customer.usecase';
+import { json } from 'sequelize';
+import CustomerPresenter from '../presenters/customer.presenter';
 
 export const customerRoute = express.Router();
 
@@ -28,11 +30,11 @@ customerRoute.post('/', async (req: Request, res: Response) => {
 
 customerRoute.get("/", async (req: Request, res: Response) => {
     const usecase = new ListCustomerUseCase(new CustomerRepository());
+    const output = await usecase.execute({});
 
-    try {
-        const output = await usecase.execute({});
-        res.send(output);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+    res.format({
+        json: async () => res.send(output),
+        xml: async () => res.send(CustomerPresenter.listXML(output)),
+    })
+
 });
