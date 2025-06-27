f35682b5a4e7287d4b1160ff76f0a11523674194
Wilson Junior <wilson.assis.junior@gmail.com>
2025-06-26 21:03:01 -0300

    Criação endpoint para Product

---
+++ Aluno/src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,73 @@
+import request from 'supertest';
+import { app, sequelize } from "../express";
+
+describe('E2E test for: Product', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it('should create a product', async () => {
+    const response = await request(app).post('/products').send({
+      name: 'Mouse',
+      price: 10,
+      type: 'a'
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.id).toEqual(expect.any(String));
+    expect(response.body.name).toBe('Mouse');
+    expect(response.body.price).toBe(10);
+  });
+
+  it('should not create a product', async () => {
+    const response = await request(app)
+      .post('/products')
+      .send({
+        name: '',
+        price: 10
+      });
+
+    expect(response.status).toEqual(500);
+  });
+
+  it("should list all products", async () => {
+
+    const response = await request(app)
+      .post('/products')
+      .send({
+        type: 'a',
+        name: 'Mouse',
+        price: 10
+      });
+
+    expect(response.status).toBe(200);
+
+    const response2 = await request(app)
+      .post('/products')
+      .send({
+        type: 'b',
+        name: 'Keyboard',
+        price: 20
+      });
+
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get('/products').send();
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.products.length).toBe(2);
+
+    const product = listResponse.body.products[0];
+    expect(product.id).toEqual(expect.any(String));
+    expect(product.name).toBe('Mouse');
+    expect(product.price).toBe(10);
+
+    const product2 = listResponse.body.products[1];
+    expect(product2.id).toEqual(expect.any(String));
+    expect(product2.name).toBe('Keyboard');
+    expect(product2.price).toBe(40);
+  });
+});
---
+++ Aluno/src/infrastructure/api/routes/product.route.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import CreateProductUseCase from '../../../usecase/product/create/create.product.usecase';
+import ProductRepository from '../../product/repository/sequelize/product.repository';
+import ListProductUseCase from '../../../usecase/product/list/list.product.usecase';
+
+export const productRoute = express.Router();
+
+productRoute.post('/', async (req: Request, res: Response) => {
+  const useCase = new CreateProductUseCase(new ProductRepository());
+
+  try {
+    const productDto = {
+      name: req.body.name,
+      price: req.body.price,
+      type: req.body.type,
+    }
+
+    const outPut = await useCase.execute(productDto);
+    res.send(outPut);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+productRoute.get('/', async (req: Request, res: Response) => {
+  const useCase = new ListProductUseCase(new ProductRepository());
+
+  try {
+    const outPut = await useCase.execute({});
+    res.send(outPut);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
